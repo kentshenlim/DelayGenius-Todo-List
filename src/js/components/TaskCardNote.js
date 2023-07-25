f451a86b0800f4a4374bc23c97ffca64b3b380b1
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-25 22:35:07 +0100

    Cannot get icon into task card note

---
+++ src/js/components/TaskCardNote.js
@@ -0,0 +1,12 @@
+export default function TaskCardNote(arr) {
+    /*
+    arr: Array of objects, [{displayString, colorString}]
+    */
+    const resNode = document.createElement('ul');
+
+    arr.forEach(element => {
+        const { disp, color } = element;
+        const li = document.createElement('li');
+
+    });
+}

bd4fc3cde87fcc5101287b357d67ffdfe9c431b4
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-26 21:26:16 +0100

    Correct when to blue the add

--- src/js/addForm.js
+++ src/js/addForm.js
@@ -2,17 +2,13 @@ import pubSub from '../utils/pubSub';
 
 export default (() => {
   // Cache DOM
+  const dateForm = document.querySelector('div.date-form');
   const textInput = document.getElementById('add-task');
   const dateInput = document.getElementById('add-date');
   const addBtn = document.getElementById('submit-task-card');
-  const dateForm = document.querySelector('div.date-form');
 
-  function clearInput() {
-    textInput.value = '';
-    dateInput.value = '';
-  }
-
-  function handleKeyDown() {
+  // Event handlers
+  function handleInputChange() {
     if (textInput.value.length) addBtn.classList.add('active');
     else addBtn.classList.remove('active');
   }
@@ -20,15 +16,17 @@ export default (() => {
   function handleClickAdd() {
     if (!textInput.value.length) return;
     pubSub.publish('add_task');
+    textInput.value = '';
+    dateInput.value = '';
+    handleInputChange();
   }
 
   function handleFocus() {
     dateForm.classList.add('active');
   }
 
-  textInput.onkeydown = handleKeyDown;
+  textInput.onkeydown = handleInputChange;
+  textInput.onkeyup = handleInputChange;
   addBtn.onclick = handleClickAdd;
   textInput.onfocus = handleFocus;
-
-  pubSub.subscribe('add_task', clearInput);
 });
